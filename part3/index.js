dabce004ac649fd551aaf4d7f817e919770d8c83
yepsamii <abrahmansw@gmail.com>
2024-09-25 01:01:35 +0600

    Add nodemon

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,6 @@
-const http = require("http");
+const express = require("express");
+const repl = require("node:repl");
+const app = express();
 
 let notes = [
   {
@@ -18,13 +20,21 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
   response.end(JSON.stringify(notes));
 });
 
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+  response.json(note);
+});
+
 const PORT = 3001;
-
-app.listen(PORT);
-
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
