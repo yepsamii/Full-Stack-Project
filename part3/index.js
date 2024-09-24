7d044a53ff9e789ec1abfb75c27b28b36f66120b
yepsamii <abrahmansw@gmail.com>
2024-09-25 01:35:30 +0600

    Add API endpoints for getting and creating notes

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const repl = require("node:repl");
 const app = express();
+
+app.use(express.json());
 
 let notes = [
   {
@@ -28,13 +29,52 @@ app.get("/api/notes", (request, response) => {
   response.end(JSON.stringify(notes));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
 
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end("Note deleted");
+});
+
 const PORT = 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
